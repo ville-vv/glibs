73f8beea4c90a1cbf9c47760da7b88449607ba3f
ville <songwei@minteachai.com>
2020-01-03 17:04:00 +0800

    vtask

--- vtask/task.go
+++ vtask/task.go
@@ -20,16 +20,17 @@ type element struct {
 }
 
 func (e *element) IncTimes() {
-
+	e.times += 1
 }
 
 type Task struct {
-	ps     bool         // 任务是否启动了
-	scheme string       // 方案名称
-	runCnt int32        // 当前任务数量
-	ts     chan element // 任务执行的单元
-	inl    time.Duration
-	cache  vqueue.Queue
+	ps      bool          // 任务是否启动了
+	scheme  string        // 方案名称
+	runCnt  int32         // 当前任务数量
+	ts      chan element  // 任务执行的单元
+	inl     time.Duration // 对执行错误的任务循环处理一次的定时
+	reLoops int           //
+	cache   vqueue.Queue  //
 }
 
 func NewTask(num ...int) *Task {
@@ -38,9 +39,10 @@ func NewTask(num ...int) *Task {
 		n = num[0]
 	}
 	return &Task{
-		ts:    make(chan element, n),
-		cache: vqueue.New(),
-		inl:   time.Second * 5,
+		ts:      make(chan element, n),
+		cache:   vqueue.New(),
+		inl:     time.Second * 5,
+		reLoops: 3,
 	}
 }
 
@@ -50,56 +52,70 @@ func (t *Task) Process(ctx context.Context) {
 		return
 	}
 
-	t.loopCache(ctx)
+	go t.loopCache(ctx)
+	go t.process(ctx)
+}
 
-	go func() {
-		t.ps = true
-		for {
-			select {
-			case elm, ok := <-t.ts:
-				if !ok {
-					t.ps = false
-					return
-				}
-				if err := elm.tf(elm.params); err != nil {
-					//任务执行失败
-					_ = t.cache.Push(elm)
-					break
-				}
-				atomic.AddInt32(&t.runCnt, -1)
-
-			case <-ctx.Done():
+func (t *Task) process(ctx context.Context) {
+	t.ps = true
+	for {
+		select {
+		case elm, ok := <-t.ts:
+			if !ok {
 				t.ps = false
 				return
 			}
+			if err := elm.tf(elm.params); err != nil {
+				//任务执行失败
+				if err = t.cache.Push(elm); err == nil {
+					break
+				}
+			}
+			atomic.AddInt32(&t.runCnt, -1)
+
+		case <-ctx.Done():
+			t.ps = false
+			return
 		}
-	}()
+	}
+}
+
+func (t *Task) digestCache() {
+	reloops := make([]element, 0, t.cache.Length())
+	for t.cache.Length() > 0 {
+		nd := t.cache.Shift()
+		if nd != nil {
+			switch elem := nd.(type) {
+			case element:
+				elem.IncTimes()
+				if err := elem.tf(elem.params); err != nil {
+					if elem.times < t.reLoops {
+						// 执行错误再次重试
+						reloops = append(reloops, elem)
+						break
+					}
+				}
+				atomic.AddInt32(&t.runCnt, -1)
+			}
+		}
+	}
+
+	// 重新放入缓存列表中
+	for i := range reloops {
+		_ = t.cache.Push(reloops[i])
+	}
 }
 
 func (t *Task) loopCache(ctx context.Context) {
-	go func() {
-		tkr := time.NewTicker(t.inl)
-
-		for {
-			for t.cache.Length() > 0 {
-				nd := t.cache.Shift()
-				if nd != nil {
-					switch elem := nd.(type) {
-					case element:
-						_ = elem.tf(elem.params)
-					}
-					atomic.AddInt32(&t.runCnt, -1)
-				}
-			}
-
-			select {
-			case <-ctx.Done():
-				return
-			case <-tkr.C:
-			}
+	tkr := time.NewTicker(t.inl)
+	for {
+		t.digestCache()
+		select {
+		case <-ctx.Done():
+			return
+		case <-tkr.C:
 		}
-	}()
-	return
+	}
 }
 
 // Add a task , the param will pass on to tf
