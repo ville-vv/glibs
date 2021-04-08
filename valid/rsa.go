c231ce14ab0e4cc2f87ba741b9dcb8b86322edb1
ville <songwei@huojintech.com>
2021-04-08 21:24:28 +0800

    update:rsa

--- valid/rsa.go
+++ valid/rsa.go
@@ -8,7 +8,6 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"errors"
-	"fmt"
 	"io"
 )
 
@@ -41,86 +40,84 @@ func GenRsaKey(bits int, pubWt io.Writer, prvWt io.Writer) error {
 }
 
 //私钥签名
-func RsaSignWithSha256(data []byte, keyBytes []byte) []byte {
+func RsaSignWithSha256(data []byte, keyBytes []byte) ([]byte, error) {
 	h := sha256.New()
 	h.Write(data)
 	hashed := h.Sum(nil)
 	block, _ := pem.Decode(keyBytes)
 	if block == nil {
-		panic(errors.New("private key error"))
+		return nil, errors.New("private key error")
 	}
 	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
 	if err != nil {
-		fmt.Println("ParsePKCS1PrivateKey err", err)
-		panic(err)
+		return nil, err
 	}
 
 	signature, err := rsa.SignPKCS1v15(rand.Reader, privateKey, crypto.SHA256, hashed)
 	if err != nil {
-		fmt.Printf("Error from signing: %s\n", err)
-		panic(err)
+		return nil, err
 	}
 
-	return signature
+	return signature, nil
 }
 
 //公钥验证
 func RsaVerySignWithSha256(data, signData, keyBytes []byte) bool {
 	block, _ := pem.Decode(keyBytes)
 	if block == nil {
-		panic(errors.New("public key error"))
+		return false
 	}
 	pubKey, err := x509.ParsePKIXPublicKey(block.Bytes)
 	if err != nil {
-		panic(err)
+		return false
 	}
 
 	hashed := sha256.Sum256(data)
 	err = rsa.VerifyPKCS1v15(pubKey.(*rsa.PublicKey), crypto.SHA256, hashed[:], signData)
 	if err != nil {
-		panic(err)
+		return false
 	}
 	return true
 }
 
 // 公钥加密
-func RsaEncrypt(data, keyBytes []byte) []byte {
+func RsaEncrypt(data, keyBytes []byte) ([]byte, error) {
 	//解密pem格式的公钥
 	block, _ := pem.Decode(keyBytes)
 	if block == nil {
-		panic(errors.New("public key error"))
+		return nil, errors.New("public key error")
 	}
 	// 解析公钥
 	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	// 类型断言
 	pub := pubInterface.(*rsa.PublicKey)
 	//加密
 	ciphertext, err := rsa.EncryptPKCS1v15(rand.Reader, pub, data)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	return ciphertext
+	return ciphertext, nil
 }
 
 // 私钥解密
-func RsaDecrypt(ciphertext, keyBytes []byte) []byte {
+func RsaDecrypt(ciphertext, keyBytes []byte) ([]byte, error) {
 	//获取私钥
 	block, _ := pem.Decode(keyBytes)
 	if block == nil {
-		panic(errors.New("private key error!"))
+		return nil, errors.New("private key error!")
 	}
 	//解析PKCS1格式的私钥
 	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	// 解密
 	data, err := rsa.DecryptPKCS1v15(rand.Reader, priv, ciphertext)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	return data
+	return data, nil
 }
